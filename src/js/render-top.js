0dd1bdd25d7f22ea50115132b8cd07269be487cb
Peter Peerdeman <peter@lifely.nl>
2019-12-30 17:14:35 +0100

    feat(top): fix compatibility with new influx based api's

--- src/js/render-top.js
+++ src/js/render-top.js
@@ -1,28 +1,53 @@
-var Config = require('config');
+var raspi = require('raspi');
 var UI = require('ui');
-var ajax = require('ajax');
 
 var RenderTop = function() {
-    ajax({
-        url: Config.RASPAPI_URL + '/api/top',
-        type:'json',
-        headers: {
-            'Authorization': Config.RASPAPI_AUTHHEADER
-        }
-    }, function(data) {
-        var resultsMenu = new UI.Menu({
-            sections: [{
-                title: 'Top',
-                items: data.slice(0,10).map(function(item) {
-                    return {
-                        title: item.COMMAND,
-                        subtitle: '%CPU: ' + item['%CPU'] + ' %MEM: ' + item['%MEM'],
-                    };
-                })
+    var topMenu = new UI.Menu({
+        sections: [{
+            items: [{
+                title: 'cpu'
+            },{
+                title: 'disk'
             }]
-        });
-        resultsMenu.show();
+        }]
     });
+
+    topMenu.on('select', function(e) {
+        if(e.itemIndex === 0) {
+            // cpu
+            raspi.get('top/cpu', function(data) {
+                var resultsMenu = new UI.Menu({
+                    sections: [{
+                        title: 'CPU',
+                        items: data.results[0].series[0].values.map(function(item) {
+                            return {
+                                title: item[1] + '% cpu',
+                                subtitle: new Date(item[0]).toLocaleString('nl')
+                            };
+                        })
+                    }]
+                });
+                resultsMenu.show();
+            });
+        } else if (e.itemIndex === 1) {
+            // cpu
+            raspi.get('top/disk', function(data) {
+                var resultsMenu = new UI.Menu({
+                    sections: [{
+                        title: 'DISK',
+                        items: data.results[0].series[0].values.map(function(item) {
+                            return {
+                                title: (item[1]/1000/1000/1000).toFixed(2) + 'GB used',
+                                subtitle: new Date(item[0]).toLocaleString('nl')
+                            };
+                        })
+                    }]
+                });
+                resultsMenu.show();
+            });
+        }
+    });
+    topMenu.show();
 };
 
 module.exports = RenderTop;
